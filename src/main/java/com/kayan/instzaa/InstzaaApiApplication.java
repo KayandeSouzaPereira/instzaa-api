2e8d6cd1151b0cf326c9a018b55a346e4bba5d9c
Kayan de Souza <40444457+KayandeSouzaPereira@users.noreply.github.com>
2024-04-25 17:27:40 -0300

    teste cors 2
    teste cors 2

--- src/main/java/com/kayan/instzaa/InstzaaApiApplication.java
+++ src/main/java/com/kayan/instzaa/InstzaaApiApplication.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
+@CrossOrigin(exposedHeaders = {"Access-Control-Allow-Origin","Access-Control-Allow-Credentials"})
 public class InstzaaApiApplication {
 
 	public static void main(String[] args) {
