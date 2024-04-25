c3267b897a9c46be859c0de4112a6b6fcec2cbd5
Kayan de Souza <40444457+KayandeSouzaPereira@users.noreply.github.com>
2024-04-25 17:18:04 -0300

    Cors alteracao
    Cors alteracao

--- src/main/java/com/kayan/instzaa/InstzaaApiApplication.java
+++ src/main/java/com/kayan/instzaa/InstzaaApiApplication.java
@@ -4,6 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class InstzaaApiApplication {
@@ -11,5 +15,14 @@ public class InstzaaApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InstzaaApiApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
+
